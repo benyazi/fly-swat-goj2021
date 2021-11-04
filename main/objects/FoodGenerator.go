84334ef9011f6ec6d3eda7bfda510bc3c1cb4dad
ben <benjamin.yazi@gmail.com>
2021-11-04 23:00:01 +0200

    added line

---
+++ main/objects/FoodGenerator.go
@@ -0,0 +1,33 @@
+components {
+  id: "FoodGeneratorCtrl"
+  component: "/main/objects/FoodGeneratorCtrl.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "factory"
+  type: "factory"
+  data: "prototype: \"/main/objects/Jam.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
