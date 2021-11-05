c158ea2e8ab7fc6271e65b93dd93502b816031de
ben <benjamin.yazi@gmail.com>
2021-11-05 17:53:24 +0200

    added combo titles

--- main/objects/FlySwapPush.go
+++ main/objects/FlySwapPush.go
@@ -130,3 +130,21 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "factory"
+  type: "factory"
+  data: "prototype: \"/main/objects/Title.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
